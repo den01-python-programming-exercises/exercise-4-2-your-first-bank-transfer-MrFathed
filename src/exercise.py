3335ad2678c4a1d3ea90b917e941f0c142e2d286
Justin Turner <jmturner638@gmail.com>
2021-08-03 02:39:06 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    matt_account = Account("Matthews account", 1000)
+    my_account = Account("My account", 0)
+
+    matt_account.withdraw(100)
+    my_account.deposit(100)
+
+    print(matt_account)
+    print(my_account)
 
 if __name__ == '__main__':
+    from account import Account
     main()
